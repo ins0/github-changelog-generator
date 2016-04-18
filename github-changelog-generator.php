ba083bb45618ba699502a4415a188a0f89e116cc
Nathan Bishop <nbish11@hotmail.com>
2016-04-19 07:40:33 +1000

    Deprecated scripts/class and refactored to use new classes. Here still for backwards compatibility.

--- github-changelog-generator.php
+++ github-changelog-generator.php
@@ -1,17 +1,67 @@
 <?php
 
+// "autoload" our classes
+if (!class_exists('ins0\GitHub\Repository')) {
+    require_once 'src/Repository.php';
+    require_once 'src/ChangelogGenerator.php';
+}
+
+use ins0\GitHub\Repository;
+use ins0\Github\ChangelogGenerator;
+use RuntimeException;
+
+/**
+ * Provides a backwords compatible wrapper for the new changelog generator.
+ *
+ * @deprecated This class is deprecated and SHOULD NOT be used.
+ * @author Marco Rieger (ins0) (Original Author)
+ * @author Nathan Bishop (nbish11) (Contributor and Refactorer)
+ * @copyright (c) 2015 Marco Rieger
+ * @license MIT
+ */
 class GithubChangelogGenerator
 {
+    /**
+     * The default file to write to.
+     *
+     * @var string
+     */
+    const DEFAULT_FILENAME = 'CHANGELOG.md'
+
+    /**
+     * @var string
+     */
+    const LABEL_TYPE_BUG = 'type_bug';
+
+    /**
+     * @var string
+     */
+    const LABEL_TYPE_FEATURE = 'type_feature';
+
+    /**
+     * @var string
+     */
+    const LABEL_TYPE_PR = 'type_pr';
+
+    /**
+     * The OAUTH token.
+     *
+     * @var string
+     */
     private $token;
-    private $fileName = 'CHANGELOG.md';
 
-    private $currentIssues;
+    /**
+     * The file we're writing the changelog to.
+     *
+     * @var string
+     */
+    private $fileName;
 
-    const LABEL_TYPE_BUG        = 'type_bug';
-    const LABEL_TYPE_FEATURE    = 'type_feature';
-    const LABEL_TYPE_PR         = 'type_pr';
-
-    /* @var array */
+    /**
+     * The sub-heading to issue label mapping.
+     *
+     * @var array
+     */
     private $issueLabelMapping = [
         self::LABEL_TYPE_BUG => [
             'bug',
@@ -19,9 +69,16 @@ class GithubChangelogGenerator
         self::LABEL_TYPE_FEATURE => [
             'enhancement',
             'feature',
-        ],
+        ]
     ];
 
+    /**
+     * Constructs an instance.
+     *
+     * @param string $token        Your GitHub OAUTH token. This is not required, but is recommended.
+     * @param array  $issueMapping Customize your issue's labels instead of using the default "bug",
+     *                             "enhancement" and "feature" labels.
+     */
     public function __construct($token = null, $issueMapping = null)
     {
         if ($issueMapping) {
@@ -29,214 +86,28 @@ class GithubChangelogGenerator
         }
 
         $this->token = $token;
+        $this->fileName = self::DEFAULT_FILENAME;
     }
 
     /**
-     * Create a changelog from given username and repository
+     * Create a changelog file for the requested repository.
      *
-     * @param $user
-     * @param $repository
-     * @param null $savePath
+     * @param string $user       The owner of the repository. Typically this is your GitHub username.
+     * @param string $repository Your GitHub repository you would like to generate the changelog file for.
+     * @param string $savePath   The path to the file. Defaults to current directory of this file.
      */
     public function createChangelog($user, $repository, $savePath = null)
     {
-        $this->currentIssues = null;
-
+        $repository = new Repository("{$user}/{$repository}", $this->token);
+        $changelogGenerator = new ChangelogGenerator($repository, $this->issueLabelMapping);
         $savePath = !$savePath ? dirname(__FILE__) . '/' . $this->fileName : null;
-        $releases = $this->collectReleaseIssues($user, $repository);
-
         $file = fopen($savePath, 'w');
-        fwrite($file, '# Change Log' . "\n\r");
-        foreach($releases as $release)
-        {
-            fwrite($file, sprintf('## [%s](%s) (%s)' . "\r\n\r\n", $release->tag_name, $release->html_url, $release->published_at));
-            $this->writeReleaseIssues($file, $release->issues);
-        }
-    }
 
-    /**
-     * Write release issues to file
-     *
-     * @param $fileStream
-     * @param $issues
-     */
-    private function writeReleaseIssues($fileStream, $issues)
-    {
-        foreach ($issues as $type => $currentIssues)
-        {
-            switch ($type)
-            {
-                case $this::LABEL_TYPE_BUG: fwrite($fileStream, '**Fixed bugs:**' . "\r\n\r\n"); break;
-                case $this::LABEL_TYPE_FEATURE: fwrite($fileStream, '**New features:**' . "\r\n\r\n"); break;
-                case $this::LABEL_TYPE_PR: fwrite($fileStream, '**Merged pull requests:**' . "\r\n\r\n"); break;
-            }
-
-            foreach ($currentIssues as $issue) {
-                fwrite($fileStream, sprintf('- %s [\#%s](%s)' . "\r\n", $issue->title, $issue->number, $issue->html_url));
-            }
-
-            fwrite($fileStream, "\r\n");
-        }
-    }
-
-    /**
-     * Collect all issues from release tags
-     *
-     * @param $user
-     * @param $repository
-     * @param null $startDate
-     * @return array
-     * @throws Exception
-     */
-    private function collectReleaseIssues($user, $repository, $startDate = null)
-    {
-        $releases = $this->callGitHubApi(sprintf('repos/%s/%s/releases', $user, $repository));
-        $data = [];
-
-        if (count($releases) <= 0) {
-            throw new \Exception('No releases found for this repository');
+        if (!$file) {
+            throw new RuntimeException(sprintf('Could not open file for writing to: %s', $savePath));
         }
 
-        do
-        {
-            $currentRelease = current($releases);
-
-            if ($startDate && date_diff(new \DateTime($currentRelease->published_at), new \DateTime($startDate))->days <= 0) {
-                continue;
-            }
-
-            $lastRelease = next($releases);
-            $lastReleaseDate = $lastRelease ? $lastRelease->published_at : null;
-            prev($releases);
-
-            $currentRelease->issues = $this->collectIssues($lastReleaseDate, $user, $repository);
-            $data[] = $currentRelease;
-
-        }while(next($releases));
-
-        return $data;
-    }
-
-    /**
-     * Collect all issues from release date
-     *
-     * @param $lastReleaseDate
-     * @param $user
-     * @param $repository
-     * @return array
-     */
-    private function collectIssues($lastReleaseDate, $user, $repository)
-    {
-        if (!$this->currentIssues) {
-            $this->currentIssues = $this->callGitHubApi(sprintf('repos/%s/%s/issues', $user, $repository), [
-                'state' => 'closed'
-            ]);
-        }
-
-        $issues = [];
-        foreach ($this->currentIssues as $x => $issue)
-        {
-            if (new \DateTime($issue->closed_at) > new \DateTime($lastReleaseDate) || $lastReleaseDate == null)
-            {
-                unset($this->currentIssues[$x]);
-
-                $type = $this->getTypeFromLabels($issue->labels);
-                if (!$type && isset($issue->pull_request)) {
-                    $type = $this::LABEL_TYPE_PR;
-                }
-
-                if ($type) {
-                    $events = $this->callGitHubApi(sprintf('repos/%s/%s/issues/%s/events', $user, $repository, $issue->number));
-                    $isMerged = false;
-
-                    foreach ($events as $event) {
-                        if(($event->event == 'merged' || $event->event == 'referenced') && !empty($event->commit_id)) {
-                            $isMerged = true;
-                            break;
-                        }
-                    }
-
-                    if ($isMerged) {
-                        $issues[$type][] = $issue;
-                    }
-                }
-            }
-        }
-
-        return $issues;
-    }
-
-    /**
-     * Get the Issue Type from Issue Labels
-     *
-     * @param $labels
-     * @return bool|int|null|string
-     */
-    private function getTypeFromLabels($labels)
-    {
-        $type = null;
-        foreach ($labels as $label)
-        {
-            if($foundLabel = $this->getTypeFromLabel($label->name)) {
-                return $foundLabel;
-            }
-        }
-
-        return null;
-    }
-
-    /**
-     * Get Type by label
-     *
-     * @param $label
-     * @param null $haystack
-     * @return bool|int|string
-     */
-    private function getTypeFromLabel($label, $haystack = null)
-    {
-        $haystack = !$haystack ? $this->issueLabelMapping : $haystack;
-        foreach($haystack as $key => $value) {
-            $current_key = $key;
-            if((is_array($value) && $this->getTypeFromLabel($label, $value) !== false) || (!is_array($value) && strcasecmp($label, $value) === 0)) {
-                return $current_key;
-            }
-        }
-        return false;
-    }
-
-    /**
-     * API call to the github api v3
-     *
-     * @param $call
-     * @param array $params
-     * @param int $page
-     * @return mixed|null
-     */
-    private function callGitHubApi($call, $params = [], $page = 1)
-    {
-        $params = array_merge(
-            $params,
-            [
-                'access_token' => $this->token,
-                'page' => $page
-            ]
-        );
-
-        $options  = [
-            'http' => [
-                'user_agent' => 'github-changelog-generator'
-            ]
-        ];
-
-        $url = sprintf('https://api.github.com/%s?%s', $call, http_build_query($params));
-        $context  = stream_context_create($options);
-        $response = file_get_contents($url, null, $context);
-        $response = $response ? json_decode($response) : [];
-
-        if(count(preg_grep('#Link: <(.+?)>; rel="next"#', $http_response_header)) === 1) {
-            return array_merge($response, $this->callGitHubApi($call, $params, ++$page));
-        }
-
-        return $response;
+        fwrite($file, $changelogGenerator->generate());
+        fclose($file);
     }
 }
